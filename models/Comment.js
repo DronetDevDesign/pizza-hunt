4afa96d82cd05cd9118d618ecdfb2183d3d9354a
Ron Dronet <ron@dronetdevdesign.com>
2022-09-18 10:32:49 -0500

    deploying

--- models/Comment.js
+++ models/Comment.js
@@ -33,12 +33,12 @@ const ReplySchema = new Schema(
 const CommentSchema = new Schema(
   {
     writtenBy: {
-      type: String
+      type: String,
       required: true,
       trim: true
     },
     commentBody: {
-      type: String
+      type: String,
       required: true,
       trim: true
     },
